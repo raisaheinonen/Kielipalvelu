1db17efc0b95461c02ea052d65adc5a40620f73e
raisaheinonen <raisa.ma.heinonen@gmail.com>
2026-08-18 11:43:10 +0300

    Ensimmäiset yritykset lisätty

---
+++ companies.js
@@ -0,0 +1,44 @@
+{
+    id:"sinervo-dentist",
+    name:"Clínica Medicodental Sinervo",
+    language:"Suomi",
+    service:"Hammaslääkäri",
+    country:"Espanja",
+    city:"Fuengirola",
+    address:"Av Ntro Padre Jesus Cautivo 2",
+    description:"Suomalainen hammaslääkäri Aurinkorannikolla.",
+    website:"www.medicodentalsinervo.com",
+    premium:false,
+    lat:36.539,
+    lng:-4.624
+},
+
+{
+    id:"sinervo-doctor",
+    name:"Clínica Medicodental Sinervo",
+    language:"Suomi",
+    service:"Lääkäri",
+    country:"Espanja",
+    city:"Fuengirola",
+    address:"Av Ntro Padre Jesus Cautivo 2",
+    description:"Suomenkielisiä lääkäripalveluja Aurinkorannikolla.",
+    website:"www.medicodentalsinervo.com",
+    premium:false,
+    lat:36.539,
+    lng:-4.624
+},
+
+{
+    id:"dental-noruega",
+    name:"Clinica Dental Noruega",
+    language:"Suomi",
+    service:"Hammaslääkäri",
+    country:"Espanja",
+    city:"Fuengirola",
+    address:"Avda. Clemente Diaz Ruiz 4",
+    description:"Suomenkielisiä hammashoitopalveluja Aurinkorannikolla.",
+    website:"www.clinicadentalnoruega.com",
+    premium:false,
+    lat:36.541,
+    lng:-4.623
+},
